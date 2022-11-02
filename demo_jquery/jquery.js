b5bf2f75efbdd851a38c0ccfd773a0f1a34af37a
basemt <112492944+basemt@users.noreply.github.com>
2022-11-02 10:18:14 -0400

    jquery demo commit

---
+++ demo_jquery/jquery.js
@@ -0,0 +1,67 @@
+// try playing with this ALERT - 
+$(document).ready(function() {alert("help! aliens are trying to invade the windows screensaver!! help bill get rid of them by clicking through the misc. items and then as a reward pick his hat :)"); })
+
+// JS files comment with two backslash lines 
+$(document).ready(function() {$("#ufo").draggable(); })
+
+$(document).ready(function() {$("#cat").draggable(); })
+
+$(document).ready(function() {
+    $("#bananas").dblclick(function(){
+    $("#ufo").fadeOut(); }); 
+    })
+
+$(document).ready(function() {$("#grapes").draggable(); })
+
+$(document).ready(function() {$("#diamond").draggable(); })
+
+$(document).ready(function() {$("#bananas").draggable(); })
+
+$(document).ready(function() {$("#bowler").draggable(); })
+
+$(document).ready(function() {$("#cowboy").draggable(); })
+
+$(document).ready(function() {$("#pirate").draggable(); })
+
+$(document).ready(function() {
+    $("#ufo").hover(function(){
+          $("#earthling").css("color", "red");
+           });
+    })
+
+    $(document).ready(function()  {
+        $("#ufo").hover(function(){
+              $("#earthling").append("we come in peace... OR DO WE?!");
+               });
+         
+        })
+        
+        $(document).ready(function()  {
+            $("#grapes").hover(function(){
+                  $("#earthling").append("NO");
+                   });
+             
+            })
+
+            $(document).ready(function()  {
+                $("#cat").hover(function(){
+                      $("#earthling").append("TRY AGAIN");
+                       });
+                 
+                })
+
+                $(document).ready(function()  {
+                    $("#diamond").hover(function(){
+                          $("#earthling").append("NO");
+                           });
+                     
+                    })
+        
+                    $(document).ready(function()  {
+                        $("#bananas").hover(function(){
+                              $("#earthling").append("YES DOUBLE CLICK QUICK!");
+                               });
+                         
+                        })
+                 
+                    
