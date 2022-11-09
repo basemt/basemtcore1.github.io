518b051cd3f030a3a0b5fb8bc86f3f1da83802ef
basemt <112492944+basemt@users.noreply.github.com>
2022-11-09 00:28:14 -0500

    anotha commit

---
+++ entries/entry8/jquery.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {alert("Happy Halloween! While I don't expect you to be seeing this on the day of or even the same month of this spooky celebration, I thought I'd give you a treat! This week, you're in control... sort of. Here are a few costumes that I've come to see over the years, some are my personal favs and others not-so favs with valid reasoning. So, sort them into the two sections as you see fit. Well, what're you waiting for? Get to spooky sorting!"); })
+
+$(document).ready(function() {$("#pirate").draggable(); })
+$(document).ready(function() {$("#cop").draggable(); })
+$(document).ready(function() {$("#vampire").draggable(); })
+$(document).ready(function() {$("#ghost").draggable(); })
+$(document).ready(function() {$("#grim").draggable(); })
+$(document).ready(function() {$("#ghostface").draggable(); })
+$(document).ready(function() {$("#clown").draggable(); })
+$(document).ready(function() {$("#queen").draggable(); })
+$(document).ready(function() {$("#shrek").draggable(); })
+$(document).ready(function() {$("#spider").draggable(); })
+$(document).ready(function() {$("#quinn").draggable(); })
+$(document).ready(function() {$("#witch").draggable(); })
+$(document).ready(function() {$("#freddy").draggable(); })
+$(document).ready(function() {$("#bacon").draggable(); })
+$(document).ready(function() {$("#zombie").draggable(); })
+$(document).ready(function() {$("#ninja").draggable(); })
+$(document).ready(function() {$("#gypsy").draggable(); })
+$(document).ready(function() {$("#cowboy").draggable(); })
+
+$(document).ready(function(){
+    $("h1").dblclick(function(){
+      alert("Well done. Now, as you know on Halloween there can never be a treat without a trick! In ur case, it's the fact that this was all for nothing :) I'm not storing your opinions or giving you brownie points for playing along, you should've already guessed this would be the outcome by now with how deep into the pages you've gotten but still... if you're surprised or let down all I can say is too bad <3 Now move on to the next entry or wallow in your despair for as long as needed.");
+    });
+  });
